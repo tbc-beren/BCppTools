8d619bad9b29de4a21d9b1b18c33bc21b43d144a
tbc-beren <beren.themoonlightshade@gmail.com>
2021-01-31 12:54:18 +0100

    Base64 functionality

---
+++ include/bcpptools/hash/bcbase64.hpp
@@ -0,0 +1,148 @@
+/**
+*
+* The Black Codex Library: Chapter: C++ Tools
+*
+* https://github.com/tbc-beren/BCppTools
+*
+* Mozilla Public License Version 2.0
+* https://github.com/tbc-beren/BCppTools/blob/master/LICENSE
+*
+* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+* If a copy of the MPL was not distributed with this file, You can obtain one at
+* https://mozilla.org/MPL/2.0/.
+*
+*/
+#pragma once
+
+#include <algorithm>
+#include <cstdint>
+#include <string>
+
+namespace bcpptools {
+namespace hash {
+
+    class BCBase64
+    {
+    private:
+        std::string mHash;
+
+    public:
+        static void fromURL(std::string& base64url) {
+            std::replace(base64url.begin(), base64url.end(), '-', '+');
+            std::replace(base64url.begin(), base64url.end(), '_', '/');
+            auto extra = base64url.size() % 4;
+            while(0 != extra--) {
+                base64url.push_back('=');
+            }
+        }
+        static void toURL(std::string& base64url) {
+            std::replace(base64url.begin(), base64url.end(), '+', '-');
+            std::replace(base64url.begin(), base64url.end(), '/', '_');
+            while(0 < base64url.size() &&
+                  base64url.back() == '=') {
+                base64url.pop_back();
+            }
+        }
+
+        void clear() {
+            mHash.clear();
+        }
+        void encode(const std::string& data) {
+            encode(castChar(data.c_str()), data.size());
+        }
+        bool decode(const std::string& data) {
+            return decode(castChar(data.c_str()), data.size());
+        }
+        const std::string& hash() const {
+            return mHash;
+        }
+        void encode(const uint8_t* buffer, size_t size) {
+            uint8_t tmp[3];
+            char     out[5];
+
+            out[4] = 0;
+
+            while(size > 0) {
+                int off = 0;
+                while (size > 0 && off < 3) {
+                    tmp[off++]=*buffer++;
+                    --size;
+                }
+
+                out[0] = (tmp[0] & 0xFC) >> 2;
+                out[1] = (tmp[0] & 0x03) << 4;
+                out[2] = 64;
+                out[3] = 64;
+
+                if (off > 1) {
+                    out[1] |= (tmp[1] & 0xF0) >> 4;
+                    out[2] =  (tmp[1] & 0x0F) << 2;
+                }
+
+                if (off > 2) {
+                    out[2] |= (tmp[2] & 0xC0) >> 6;
+                    out[3] = (tmp[2] & 0x3F);
+                }
+
+                out[0] = charToHash(out[0]);
+                out[1] = charToHash(out[1]);
+                out[2] = charToHash(out[2]);
+                out[3] = charToHash(out[3]);
+
+                mHash.append(out);
+            }
+        }
+        bool decode(const uint8_t* buffer, size_t size) {
+            uint8_t tmp[5];
+            char    out[4];
+
+            if (size%4 != 0) {
+                return false;
+            }
+
+            out[3]=0;
+
+            while(size > 0) {
+                int off = 0;
+                while (size > 0 && off < 4) {
+                    char chr = *buffer++;
+                    char val = chr == '='? 0 : charHash(chr);
+                    if (val == -1) {
+                        return false;
+                    }
+                    tmp[off++]=val;
+                    --size;
+                }
+
+                out[0] = ((tmp[0] & 0x3F) << 2) | ((tmp[1] & 0x30)>>4);
+                out[1] = ((tmp[1] & 0x0F) << 4) | ((tmp[2] & 0x3C)>>2);
+                out[2] = ((tmp[2] & 0x03) << 6) | tmp[3];
+
+                this->mHash.append(out);
+            }
+
+            return true;
+        }
+    private:
+        const uint8_t* castChar(const char* data) {
+            return reinterpret_cast<const uint8_t*>(data);
+        }
+        static const std::string& hashTranslateTable() {
+            static const std::string hashTable = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+            return hashTable;
+        }
+        static char charHash(char v) {
+            const auto& hashTable = hashTranslateTable();
+            auto pos = hashTable.find_first_of(v);
+            if (hashTable.npos == pos) {
+                return -1;
+            }
+            return pos & 0xFF;
+        }
+        static char charToHash(uint8_t chr) {
+            return hashTranslateTable().at(chr);
+        }
+    };
+
+} // namespace hash
+} // namespace bcpptools
---
+++ test/testHashBase64.cpp
@@ -0,0 +1,91 @@
+/**
+*
+* The Black Codex Library: Chapter: C++ Tools
+*
+* https://github.com/tbc-beren/BCppTools
+*
+* Mozilla Public License Version 2.0
+* https://github.com/tbc-beren/BCppTools/blob/master/LICENSE
+*
+* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+* If a copy of the MPL was not distributed with this file, You can obtain one at
+* https://mozilla.org/MPL/2.0/.
+*
+*/
+#include <bcpptools/hash/bcbase64.hpp>
+
+#include <gtest/gtest.h>
+
+#include <string>
+#include <vector>
+
+TEST(Base64, decodeInvalidLen) {
+    using namespace bcpptools::hash;
+    EXPECT_FALSE(BCBase64().decode("X"));
+    EXPECT_FALSE(BCBase64().decode("XX"));
+    EXPECT_FALSE(BCBase64().decode("XXX"));
+    EXPECT_TRUE (BCBase64().decode("XXXX"));
+    EXPECT_FALSE(BCBase64().decode("XXXXX"));
+}
+TEST(Base64, decodeInvalidChar) {
+    using namespace bcpptools::hash;
+    EXPECT_FALSE(BCBase64().decode("----"));
+}
+TEST(Base64, encodeAndDecode) {
+    std::vector<std::pair<std::string, std::string>> testEntries = {
+        {"\370\017\301",        "+A/B"},
+        {"x",                   "eA=="},
+        {"Message-Hash",        "TWVzc2FnZS1IYXNo"},
+        {"MessageToHash",       "TWVzc2FnZVRvSGFzaA=="},
+        {"MessageForHashing",   "TWVzc2FnZUZvckhhc2hpbmc="},
+    };
+
+    for (const auto& item: testEntries) {
+        bcpptools::hash::BCBase64 b64;
+        b64.encode(item.first);
+
+        const std::string output = b64.hash();
+        EXPECT_EQ(item.second, output);
+
+        b64.clear();
+        b64.decode(output);
+        EXPECT_EQ(item.first, b64.hash());
+    }
+}
+TEST(Base64, encodeAndDecodeURL) {
+    using namespace bcpptools::hash;
+
+    BCBase64 b;
+    b.decode("+===");
+    std::string out = b.hash();
+
+    uint8_t txt[5];
+    txt[0] = out[0];
+    txt[1] = out[1];
+    txt[2] = out[2];
+    txt[3] = out[3];
+    txt[4] = 0;
+
+    (void)txt;
+
+    std::vector<std::pair<std::string, std::string>> testEntries = {
+        {"\370\017\301",        "-A_B"},
+        {"x",                   "eA"},
+    };
+
+    for (const auto& item: testEntries) {
+        BCBase64 b64;
+        b64.encode(item.first);
+
+        std::string output = b64.hash();
+        BCBase64::toURL(output);
+
+        EXPECT_EQ(item.second, output);
+
+        BCBase64::fromURL(output);
+
+        b64.clear();
+        b64.decode(output);
+        EXPECT_EQ(item.first, b64.hash());
+    }
+}
